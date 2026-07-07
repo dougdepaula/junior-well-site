9530e048e070e49b0f1fd827244f8568fa6a09fe
dougdepaula <dougdepaula@outlook.com>
2026-07-06 22:06:26 -0300

    chore: limpeza total de configuracoes de servidor e build

--- dist/client/server/assets/_tanstack-start-manifest_v-REyz91d5.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/dougd/Desktop/junior-well-site/src/routes/__root.tsx", children: ["/", "/privacidade", "/termos"], preloads: ["/assets/index-D_uq-qVm.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-D_uq-qVm.js" } }] }, "/": { filePath: "C:/Users/dougd/Desktop/junior-well-site/src/routes/index.tsx", children: void 0, css: ["/assets/index-C2J0D0kV.css"], preloads: ["/assets/index-Ca6ItfWt.js"] } } });
-export {
-  tsrStartManifest
-};
